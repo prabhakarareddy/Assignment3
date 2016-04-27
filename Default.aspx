807d09d758cf1bf9c79e925704a38a86c97b4427
prabhakarareddy <kprfor.net@gmail.com>
2016-04-27 16:53:26 +0530

    cfsd
    dfgdfg

---
+++ Default.aspx
@@ -0,0 +1,117 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+#header {
+    background-color:cornflowerblue;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+#nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:100px;
+    float:left;
+    padding:5px; 
+}
+#section {
+    width:350px;
+    float:left;
+    padding:10px; 
+}
+#footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+    padding:5px; 
+}
+</style>
+</head>
+<body>
+    <div id="header">
+<h1>User Information Application</h1>
+</div>
+    <form id="form1" runat="server" style="padding-top:50px">
+    <table style="background-color:papayawhip;" align="center">
+        <tr><td>Create UserType</td></tr>
+        <tr>
+            <td>
+            <asp:DropDownList ID="ddlUserType" runat="server" AutoPostBack="true" OnTextChanged="Unnamed_TextChanged">
+<asp:ListItem>Select</asp:ListItem>
+            <asp:ListItem>Student</asp:ListItem>
+            <asp:ListItem>Teachers</asp:ListItem>
+            <asp:ListItem>Professors</asp:ListItem>
+            </asp:DropDownList>
+            </td>
+        </tr>
+
+        <tr><td>Specife a Username</td></tr>
+        <tr>
+            <td>
+           <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox><asp:Label ID="lblDisplay" runat="server"></asp:Label>
+            </td>
+             <td>
+           <asp:Button ID="btnFindUser" text="Find User" runat="server" OnClick="btnFindUser_Click"></asp:Button>
+            </td>
+        </tr>
+
+        <tr><td>Specife a password</td></tr>
+        <tr>
+            <td>
+           <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr><td>Specife a password</td></tr>
+        <tr>
+            <td>
+           <asp:CheckBox ID="chkDefaule" runat="server" Text="Reset To the Default Password" />
+            </td>
+        </tr>
+
+        <tr><td>Specife an email</td></tr>
+        <tr>
+            <td>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+
+         <tr><td>Choose a email Extension</td></tr>
+        <tr>
+            <td>
+            <asp:RadioButtonList id="emailType" runat="server">
+                <asp:ListItem Text="@robertmorris.edu" value="@robertmorris.edu"></asp:ListItem>
+                <asp:ListItem Text="@gmail.com" value="@robertmorris.edu"></asp:ListItem>
+                <asp:ListItem Text="@yahoo.com" value="@robertmorris.edu"></asp:ListItem>
+                <asp:ListItem Text="@hotmail.com" value="@robertmorris.edu"></asp:ListItem>                
+            </asp:RadioButtonList>
+            </td>
+        </tr>
+
+        <tr><td>Enter Miscellaneous instructions:</td></tr>
+        <tr>
+            <td>
+           <asp:TextBox ID="txtNode" runat="server" TextMode="MultiLine"></asp:TextBox>
+            </td>
+        </tr>
+         <tr>
+            <td>
+          <asp:Label ID="lblStatus" runat="server" ForeColor="Red"></asp:Label>
+            </td>
+        </tr>
+        <tr><td><asp:Button ID="btnCreatUser" runat="server" text="Create User" style="padding-left:10px" OnClick="btnCreatUser_Click"/></td>
+            <td><asp:Button ID="btnUpdate" runat="server" text="Update" align="center" OnClick="btnUpdate_Click" /></td>
+            <td><asp:Button ID="btnDelete" runat="server" text="Delete" align="center" OnClick="btnDelete_Click" /></td>
+        </tr>            
+    </table>
+    
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace Assignment3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["dbCOn"].ConnectionString;
+            if (!Page.IsPostBack)
+            {
+                btnDelete.Visible = false;
+                btnUpdate.Visible = false;
+            }
+            conn = new SqlConnection(con);
+            conn.Open();
+        }
+
+        protected void Unnamed_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreatUser_Click(object sender, EventArgs e)
+        {            
+            string con= ConfigurationManager.ConnectionStrings["dbCOn"].ConnectionString;
+
+            string email = txtEmail.Text + emailType.SelectedValue;
+
+            conn = new SqlConnection(con);            
+            conn.Open();
+            cmd = new SqlCommand("insert into users values(" + "'" + txtUserName.Text + "'"+",'" + txtPassword.Text + "'," + "'" + email + "'" +",'" + ddlUserType.SelectedValue + "','" + txtNode.Text+ "')", conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                lblStatus.Text = "New Record Created Succesfully";
+
+                txtUserName.Text = "";
+                txtPassword.Text = "";
+                ddlUserType.SelectedValue = "Select";
+                txtNode.Text = "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        protected void btnFindUser_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["dbCOn"].ConnectionString;
+
+            conn = new SqlConnection(con);
+            conn.Open();
+            cmd = new SqlCommand("select * from users where username = '" + txtUserName.Text + "'", conn);
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                
+                DataSet customers = new DataSet();
+                adapter.Fill(customers, "Users");
+
+                DataTable dt = customers.Tables["Users"];
+                if (dt.Rows.Count > 0)
+                {
+                    txtPassword.Text = dt.Rows[0][1].ToString();
+                    txtNode.Text = dt.Rows[0][4].ToString();
+                    string email = dt.Rows[0][2].ToString();
+                    String[] elements = Regex.Split(email, "@");
+                    txtEmail.Text = elements[0];
+                    emailType.SelectedValue = "@" + elements[1].ToString();
+                    ddlUserType.SelectedValue = dt.Rows[0][3].ToString();
+                    btnCreatUser.Visible = false;
+                    btnDelete.Visible = true;
+                    btnUpdate.Visible = true;                 
+                }
+                else
+                {
+                    lblDisplay.Text = "No User Found";
+                    lblStatus.Text = "Please Create New Record";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["dbCOn"].ConnectionString;
+
+            string email = txtEmail.Text + emailType.SelectedValue;
+
+            conn = new SqlConnection(con);
+            conn.Open();
+            cmd = new SqlCommand("update users set username =" + "'" + txtUserName.Text + "'" + ",password= '" + txtPassword.Text + "',Email=" + "'" + email + "'" + ",userType='" + ddlUserType.SelectedValue + "',note='" + txtNode.Text + "'", conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                btnCreatUser.Visible = true;
+                btnDelete.Visible = false;
+                btnUpdate.Visible = false;
+                txtUserName.Text = "";
+                txtPassword.Text = "";
+                ddlUserType.SelectedValue = "Select";
+                txtNode.Text = "";
+                lblStatus.Text = "Record Updated Succesufully";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["dbCOn"].ConnectionString;
+
+            string email = txtEmail.Text + emailType.SelectedValue;
+
+            conn = new SqlConnection(con);
+            conn.Open();
+            cmd = new SqlCommand("delete from users where username=" +"'" + txtUserName.Text + "'", conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                btnCreatUser.Visible = true;
+                btnUpdate.Visible = false;
+                btnDelete.Visible = false;
+                lblStatus.Text = "Record Deleted Succesufully";
+
+                txtUserName.Text = "";
+                txtPassword.Text = "";
+                ddlUserType.SelectedValue = "Select";
+                txtNode.Text = "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
